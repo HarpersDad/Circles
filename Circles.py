d36d2ed511ae25011441d33c04f865aa589a9c7b
Clifton Lindsey <42348662+HarpersDad@users.noreply.github.com>
2023-08-25 09:48:49 -0400

    Update Circles.py

--- Circles.py
+++ Circles.py
@@ -1,5 +1,13 @@
 import math
 
+def isItASphere(r):
+
+    volume = (4 / 3) * math.pi * r * r * r
+    surfaceArea = 4 * math.pi * r * r
+
+    print(f"\nIf this is a sphere, it's volume is approximately {round(volume, 2)},")
+    print(f"and it's surface area is approximately {round(surfaceArea, 2)}")
+
 def getCircumference(r):
 
     circumference = 2 * math.pi * r
@@ -10,13 +18,14 @@ def getArea(r):
 
     area = math.pi * r * r
 
-    print(f"The area of your circle is: {round(area, 2)}")
+    print(f"\nThe area of your circle is: {round(area, 2)}")
 
 def main():
     radius = input("Enter the radius: ")
 
-    getArea(int (radius))
-    getCircumference(int (radius))
+    getArea(int(radius))
+    getCircumference(int(radius))
+    isItASphere(int(radius))
 
 
 main()
